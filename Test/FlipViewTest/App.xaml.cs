2913bb107be2ac161a558cd5c5b478034dbda1da
Jon2G <jonyjouna@gmail.com>
2025-02-01 22:41:58 -0600

    Add target for net 9

--- Test/FlipViewTest/App.xaml.cs
+++ Test/FlipViewTest/App.xaml.cs
@@ -1,12 +1,16 @@
-﻿namespace FlipViewTest
+﻿
+namespace FlipViewTest
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+        }
 
-            MainPage = new AppShell();
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
         }
     }
 }
